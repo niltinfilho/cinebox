a24610b9b05cdc4817fd526b35b6a72930905e76
Nilton Filho <niltonfilho115@gmail.com>
2025-08-12 04:36:36 -0300

    feat: add riverpod config to splash screen

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends ConsumerStatefulWidget {
+  const SplashScreen({super.key});
 
-  const SplashScreen({ super.key });
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _SplashScreenState();
+}
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Splash Screen'),),
-           body: Container(),
-       );
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash Screen Riverpod'),
+      ),
+      body: Container(),
+    );
   }
 }
