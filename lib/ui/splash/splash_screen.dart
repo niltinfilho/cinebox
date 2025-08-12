72983ed16cdd805832b1424eac15c5e9d11b8883
Nilton Filho <niltonfilho115@gmail.com>
2025-08-12 12:23:45 -0300

    feat: build splash screen

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -15,21 +15,22 @@ class _SplashScreenState extends ConsumerState<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash Screen Riverpod'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          showLoader();
-          await Future.delayed(Duration(seconds: 2));
-          hideLoader();
-          showSuccessSnackBar('Sucesso qualquer');
-          showErrorSnackBar('Erro qualquer');
-          showInfoSnackBar('Indo qualquer');
-        },
-      ),
-      body: Center(
-        child: Image.asset(R.ASSETS_IMAGES_BG_LOGIN_PNG),
+      body: Stack(
+        children: [
+          Image.asset(
+            R.ASSETS_IMAGES_BG_LOGIN_PNG,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            constraints: BoxConstraints.expand(),
+            color: Colors.black.withAlpha(170),
+          ),
+          Center(
+            child: Image.asset(R.ASSETS_IMAGES_LOGO_PNG),
+          ),
+        ],
       ),
     );
   }
