7b47ef1532d6b68b68a5c09abd023504bc252f8c
Nilton Filho <niltonfilho115@gmail.com>
2025-08-13 09:05:10 -0300

    feat: connecting googleSignIn

--- lib/data/services/services_providers.dart
+++ lib/data/services/services_providers.dart
@@ -1,3 +1,5 @@
+import 'package:cinebox/data/services/google_signin/google_signin_service.dart';
+import 'package:cinebox/data/services/google_signin/google_signin_service_impl.dart';
 import 'package:cinebox/data/services/local_storage/local_storage_service.dart';
 import 'package:cinebox/data/services/local_storage/local_storage_service_impl.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -6,6 +8,11 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'services_providers.g.dart';
 
 @riverpod
+GoogleSigninService googleSigninService(Ref ref) {
+  return GoogleSigninServiceImpl();
+}
+
+@Riverpod(keepAlive: true)
 LocalStorageService localStorageService(Ref ref) {
   return LocalStorageServiceImpl(flutterSecureStorage: FlutterSecureStorage());
 }
