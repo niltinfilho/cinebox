4d9fadcdbf483fe062504cfb818f9731907d0f4d
Nilton Filho <niltonfilho115@gmail.com>
2025-08-14 12:00:10 -0300

    feat: bottom nav bar

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cinebox/ui/home/widgets/home_bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,9 +13,18 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Finalmente estamos na página principal'),
+      extendBody: true,
+      body: AnimatedSwitcher(
+        duration: Duration(milliseconds: 400),
+        transitionBuilder: (child, animation) => FadeTransition(
+          opacity: animation,
+          child: child,
+        ),
+        child: Container(
+          color: Colors.red,
+        ),
       ),
+      bottomNavigationBar: HomeBottomNavBar(),
     );
   }
 }
