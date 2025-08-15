cc9aad0d99606fc5bb00de427d72b13c42675206
Nilton Filho <niltonfilho115@gmail.com>
2025-08-15 20:31:21 -0300

    feat: movies by search screen

---
+++ lib/ui/movies/widgets/movies_by_search.dart
@@ -0,0 +1,24 @@
+import 'package:cinebox/ui/movies/widgets/movies_box.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MoviesBySearch extends ConsumerStatefulWidget {
+  const MoviesBySearch({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _MoviesBySearchState();
+}
+
+class _MoviesBySearchState extends ConsumerState<MoviesBySearch> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 140),
+      child: MoviesBox(
+        title: 'Filmes encontrados pela busca',
+        vertical: true,
+      ),
+    );
+  }
+}
